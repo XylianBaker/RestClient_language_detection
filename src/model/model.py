90ea853420e1f43d51d1ce6fabf54b3b9e4478a1
Jan Kammellander <janschwiderik@gmail.com>
2023-02-27 12:53:14 +0100

    :bulb: added documentation in model.py

--- src/model/model.py
+++ src/model/model.py
@@ -2,11 +2,23 @@ import requests
 
 
 def language_detection(text: str):
-    base_url = "http://127.0.0.1:5000/detect"
-    response = requests.get(base_url, params={"text": text})
-    response = response.json()
-    # make the response, which looks like this: ["Somali",99.99984369234461,true,"so"]
-    # into a string like this: "reliable: yes, language: Somali, probability: 99.99984369234461"
+    """
+    Detects the language of the input text using the Flask API at http://127.0.0.1:5000/detect.
+
+    Parameters:
+    -----------
+    text : str
+        The input text whose language needs to be detected.
+
+    Returns:
+    --------
+    str
+        A formatted string containing the reliability, language, and probability of the detected language.
+    """
+    base_url = "http://127.0.0.1:5000/detect"  # The base URL of the Flask API
+    response = requests.get(base_url, params={"text": text})  # Send a GET request to the API to detect the language
+    response = response.json()  # Convert the response to JSON format
+    # Format the response as a string containing the reliability, language, and probability of the detected language
     return f"reliable: <b>{response[2]}</b><br/>language: <b>{response[0]}</b><br/>probability: <b>{response[1]}%<b/>"
 
 
